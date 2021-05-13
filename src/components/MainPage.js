4cef12a7f9e59e88dfb97bd6e23ef91de93918ef
BuildTools <joel.sj@live.se>
2021-05-14 01:58:20 +0200

    Mainpage styling, font imports and env example

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -2,7 +2,7 @@ import {Link} from "react-router-dom"
 
 export default function MainPage() {
     return (
-        <div>
+        <div className="startpage-container">
             <h1 className="title">Motivizer</h1>
             <Link to="/motivate">Generate Motivation</Link>
         </div>
--- src/components/MotivatePage.js
+++ src/components/MotivatePage.js
@@ -1,9 +1,10 @@
+import {Link} from "react-router-dom"
 import PosterGenerator from "./PosterGenerator"
 
 export default function MotivatePage() {
     return (
         <div className="motivate-container">
-            <h1>Motivizer</h1>
+            <Link to="/"><h1>Motivizer</h1></Link>
             <PosterGenerator />
         </div>
     )
