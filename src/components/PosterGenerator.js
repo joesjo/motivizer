3945880b08434b24ea834a8267b98d2dbcaed285
BuildTools <joel.sj@live.se>
2021-05-13 16:20:35 +0200

    Added axios for fetching

--- src/components/PosterGenerator.js
+++ src/components/PosterGenerator.js
@@ -1,6 +1,15 @@
+import {useState} from "react"
+
 import Poster from "./Poster"
 
 export default function PosterGenerator() {
+    const [quotes, setQuotes] = useState([])
+    const [image, setImage] = useState('')
+
+    const fetchQuotes = () => {
+        
+    }
+
     return (
         <div className="poster-container" >
             <h2>Generator</h2>
