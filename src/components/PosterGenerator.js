b5d4b77b4847c10e5a53d183d2c490160cdeb3e7
BuildTools <joel.sj@live.se>
2021-05-29 16:52:32 +0200

    Small layout change

--- src/components/PosterGenerator.js
+++ src/components/PosterGenerator.js
@@ -9,6 +9,9 @@ export default function PosterGenerator() {
     const [loading, setLoading] = useState(false)
 
     const getPosterInfo = () => {
+        setQuote({})
+        setImage('')
+
         setLoading(true)
 
         Promise.all([
@@ -41,6 +44,8 @@ export default function PosterGenerator() {
         .catch(err => {
             console.log(err)
         })
+
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -48,11 +53,13 @@ export default function PosterGenerator() {
     }, [])
 
     return (
-        <div>
+        <>
+            {loading ? <p>Loading...</p> : null}
             <Poster quoteContent={quote.content} quoteAuthor={quote.author} imageLink={image}/>
             <div className="button-container">
-                <button onClick={getPosterInfo} disabled={loading}>Generate New</button>
+                <button onClick={() => getPosterInfo()}>Generate New</button>
+                {/*<button>Unique-ify</button> TODO: later implementation */}
             </div>
-        </div>
+        </>
     )
 }
